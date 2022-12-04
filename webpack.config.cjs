8dd05b1e56624adddcc65e444faa1a3bd2d187e9
Linus Thiel <linus.thiel@bonniernews.se>
2022-12-04 21:26:23 +0100

    Build ES module and CommonJS module
    - Define two outputs, one targeting es module and one commonjs
    - Export them in package.json for import and require, respectively

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,15 +1,9 @@
 const path = require('path')
 
-module.exports = {
+const common = {
   devtool: false,
   entry: {
     app: './src/index.js',
-  },
-  output: {
-    library: 'index',
-    libraryTarget: 'commonjs2',
-    filename: 'index.js',
-    hashFunction: 'xxhash64',
   },
   resolve: {
     modules: [__dirname, 'node_modules', 'src'],
@@ -18,15 +12,28 @@ module.exports = {
   resolveLoader: {
     modules: [path.join(__dirname, 'node_modules')],
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: ['babel-loader'],
-        include: [path.join(__dirname, 'src')],
-        exclude: /node_modules/,
-      },
-    ],
-  },
   target: 'node',
 }
+
+module.exports = [
+  {
+    ...common,
+    output: {
+      libraryTarget: 'commonjs2',
+      filename: 'cjs/index.js',
+      hashFunction: 'xxhash64',
+      chunkFormat: 'commonjs',
+    },
+  }, {
+    ...common,
+    experiments: {
+      outputModule: true,
+    },
+    output: {
+      libraryTarget: 'module',
+      filename: 'mjs/index.js',
+      hashFunction: 'xxhash64',
+      chunkFormat: 'module',
+    },
+  }
+]
