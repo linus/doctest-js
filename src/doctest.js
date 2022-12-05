dacf9ef89e5858d3f0f46540667d62bc9aeea4eb
Linus Thiel <linus.thiel@bonniernews.se>
2022-12-05 14:29:28 +0100

    Run each doctest promise sequentially

--- src/doctest.js
+++ src/doctest.js
@@ -12,20 +12,18 @@ const defaultTestingFunction = (actual, expected, doctest) => {
   })
 }
 
-export default (filePath, options = {}) => {
+export default async (filePath, options = {}) => {
   const file = fs.readFileSync(filePath, 'utf8')
   const doctests = parseDoctests(file)
-  const evalPromises = doctests.map((doctest) => evalDoctest(doctest, filePath, options.instance))
-  return Promise.all(evalPromises).then(results => {
-    results.forEach(({ actual, expected }, index) => {
-      if (actual.error) {
-        throw actual.error
-      } else if (expected.error) {
-        throw expected.error
-      } else {
-        const { testingFunction = defaultTestingFunction } = options
-        testingFunction(actual, expected, doctests[index], index)
-      }
-    })
-  })
+  for (const [index, doctest] of doctests.entries()) {
+    const { actual, expected } = await evalDoctest(doctest, filePath, options.instance);
+    if (actual.error) {
+      throw actual.error
+    } else if (expected.error) {
+      throw expected.error
+    } else {
+      const { testingFunction = defaultTestingFunction } = options
+      testingFunction(actual, expected, doctests[index], index)
+    }
+  }
 }
